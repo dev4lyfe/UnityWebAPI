ca111162e0061e12e7743e2f4a88ac0e6c01fd17
Pablo Leon-Luna <pablo.leon-luna@scopely.com>
2020-01-07 17:57:47 -0800

    added in player controller and player

---
+++ BugReporterUnityWebAPI/Contracts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Unity3d_Restful_Service.Contracts
+{
+    [JsonObject, Serializable]
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public float Score { get; set; }
+    }
+}
---
+++ BugReporterUnityWebAPI/Controllers/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BugReporterUnityWebAPI.Contracts;
+
+namespace Unity3d_Restful_Service.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private static List<Player> Players = new List<Player>()
+        {
+            new Player
+            {
+                Id = 1,
+                FullName = "Guy Bob",
+                Score = 111
+            },
+            new Player
+            {
+                Id = 2,
+                FullName = "Guy Joe",
+                Score = 222
+            },
+            new Player
+            {
+                Id = 3,
+                FullName = "Guy Ren",
+                Score = 333
+            },
+        };
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json(Players);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Player player = Players.Single(p => p.Id == id);
+            return Json(player);
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody]Player newPlayer)
+        {
+            Players.Add(newPlayer);
+            return Json(Players);
+        }
+
+        [HttpPut("{id}")]
+        public JsonResult Put(int id, [FromBody]float newScore)
+        {
+            Player player = Players.Single(p => p.Id == id);
+            player.Score = newScore;
+            return Json(player);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            Player player = Players.Single(p => p.Id == id);
+            Players.Remove(player);
+        }
+    }
+}
+
